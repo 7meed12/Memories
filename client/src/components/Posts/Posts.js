7933082d5b27b58c1647d80bad4e4dafedb8b975
7meed12 <7meedo12@gmail.com>
2022-05-20 02:40:48 +0200

    redux setup

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Post from'./Post/post';
+import useStyles from './styles'
+
+const Posts=()=>{
+    const classes = useStyles()
+    return(
+        <>
+        <h1>Posts</h1>
+        <Post/>
+        </>
+    )
+}
+export default Posts;
